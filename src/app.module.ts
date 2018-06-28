30293375fdfcc1e844513448fd787055fba3ad70
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-06-28 13:09:07 +1000

    stitch local and prisma graphql api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,32 @@
-import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { Module, Inject, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { graphqlExpress } from 'apollo-server-express';
 import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
 import { UserModule } from './user/UserModule';
 import expressPlayground from 'graphql-playground-middleware-express';
+import { mergeSchemas } from 'graphql-tools';
+import { remoteSchema } from './remoteSchema.provider';
+import { GraphQLSchema } from 'graphql';
 
 @Module({
   imports: [GraphQLModule, UserModule],
+  providers: [remoteSchema],
 })
 export class AppModule implements NestModule {
-  constructor(private readonly graphQLFactory: GraphQLFactory) {}
+  constructor(
+    private readonly graphQLFactory: GraphQLFactory,
+    @Inject('PrismaSchema') private readonly remoteSchema: GraphQLSchema,
+  ) {}
 
   configure(consumer: MiddlewareConsumer) {
-    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql');
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths(
+      './src/**/*.graphql',
+    );
     const schema = this.graphQLFactory.createSchema({ typeDefs });
-
+    const mergedSchema = mergeSchemas({ schemas: [schema, this.remoteSchema] });
     consumer
       .apply(expressPlayground({ endpoint: '/graphql' }))
       .forRoutes('/playground')
-      .apply(graphqlExpress(req => ({ schema, rootValue: req })))
+      .apply(graphqlExpress(req => ({ schema: mergedSchema, rootValue: req })))
       .forRoutes('/graphql');
   }
 }
