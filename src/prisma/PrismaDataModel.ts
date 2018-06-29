948f746060695abe7784c3699141b45c6d93ce5f
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-06-29 15:54:46 +1000

    deploy prisma service after new type was added

--- src/prisma/PrismaDataModel.ts
+++ src/prisma/PrismaDataModel.ts
@@ -1,26 +1,35 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Logger } from '@nestjs/common';
 import * as fs from 'fs';
 import * as util from 'util';
+import { spawn } from 'child-process-promise';
 import outdent from 'outdent';
 
 const appendFile = util.promisify(fs.appendFile);
 const readFile = util.promisify(fs.readFile);
+
 @Injectable()
 export class PrismaDataModel {
   private static readonly datamodelPath = './prisma/datamodel.graphql';
-  constructor(@Inject('PrismaDatamodel') private dataModel: Buffer) {}
+  constructor(@Inject('PrismaDatamodel') private dataModel: Buffer, private readonly logger: Logger) {}
 
   async addType(typeName: string) {
-    if (!this.typeExists(typeName)) {
-      const typeTemplate = outdent`\n
-      type ${typeName} {
-        id: ID! @unique
-      }`;
-      appendFile(PrismaDataModel.datamodelPath, typeTemplate);
-    } else {
-      throw new Error('Type already exists');
+    if (this.typeExists(typeName)) throw new Error('Type already exists');
+    try {
+      await this.addTypeToDatamodel(typeName);
+      await this.deploy();
+      await this.reloadDatamodel();
+    } catch(err) {
+      this.logger.error(err);
+      throw new Error('Type cannot be added');
     }
-    await this.reloadDatamodel();
+  }
+
+  private async addTypeToDatamodel(typeName: string) {
+    const typeTemplate = outdent`\n
+    type ${typeName} {
+      id: ID! @unique
+    }`;
+    return appendFile(PrismaDataModel.datamodelPath, typeTemplate);
   }
 
   private typeExists(typeName: string): boolean {
@@ -31,5 +40,9 @@ export class PrismaDataModel {
   private async reloadDatamodel() {
     const fileContent = await readFile(PrismaDataModel.datamodelPath);
     this.dataModel = fileContent;
+  }
+
+  private async deploy() {
+    return spawn('prisma', ['deploy', 'f']);
   }
 }
--- src/prisma/PrismaModule.ts
+++ src/prisma/PrismaModule.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { remoteSchema } from './remoteSchema.provider';
 import { dataModel } from './dataModel.provider';
 import { PrismaDataModel } from './PrismaDataModel';
 
 @Module({
-  providers: [remoteSchema, dataModel, PrismaDataModel],
+  providers: [remoteSchema, dataModel, PrismaDataModel, Logger],
   exports: [remoteSchema, PrismaDataModel],
 })
 export class PrismaModule {}
