3073055d51527d6670e212fb1cd4715891958dcd
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-06-30 17:21:36 +1000

    replace graphql playground with graphi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module, Inject, MiddlewareConsumer, NestModule } from '@nestjs/common';
-import { graphqlExpress } from 'apollo-server-express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
 import { UserModule } from './user/UserModule';
-import expressPlayground from 'graphql-playground-middleware-express';
 import { mergeSchemas } from 'graphql-tools';
 import { GraphQLSchema } from 'graphql';
 import { ContentTypeModule } from './content-type/ContentTypeModule';
@@ -23,8 +22,8 @@ export class AppModule implements NestModule {
     const schema = this.graphQLFactory.createSchema({ typeDefs });
     const mergedSchema = mergeSchemas({ schemas: [schema, this.remoteSchema] });
     consumer
-      .apply(expressPlayground({ endpoint: '/graphql' }))
-      .forRoutes('/playground')
+      .apply(graphiqlExpress({ endpointURL: '/graphql' }))
+      .forRoutes('/graphi')
       .apply(
         graphqlExpress(req => ({
           schema: mergedSchema,
