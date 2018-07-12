4582ace2f8dd8c2458a3be77cff697dd9160900c
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-07-12 10:49:42 +1000

    Test add content type functionality

---
+++ src/prisma/PrismaDataModel.spec.ts
@@ -0,0 +1,48 @@
+import { dataModels } from './dataModels.provider';
+import { PrismaDataModel } from './PrismaDataModel';
+import { Test } from '@nestjs/testing';
+import * as fs from 'fs';
+import * as util from 'util';
+
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+describe('PrismaDataModel', () => {
+  let prismaDataModel: PrismaDataModel;
+  const contentTypeDataModelPath = './test/resources/contentTypes.graphql.txt';
+  let initialDatamodelContent: Buffer;
+
+  beforeAll(async () => {
+    initialDatamodelContent = await readFile(contentTypeDataModelPath);
+  });
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        PrismaDataModel,
+        {
+          provide: 'ContentTypeDataModelPath',
+          useValue: contentTypeDataModelPath,
+        },
+        {
+          provide: 'ContentTypesDatamodel',
+          useValue: '',
+        },
+      ],
+    }).compile();
+
+    prismaDataModel = module.get<PrismaDataModel>(PrismaDataModel);
+  });
+
+  describe('addType', () => {
+    it('should add a new type to the data model', async () => {
+      await prismaDataModel.addType('photo');
+      const expectedContent = await readFile('./test/resources/contentTypes.addType_1.graphql.txt');
+      expect(prismaDataModel.contentTypeDataModel.toString()).toBe(expectedContent.toString());
+    });
+  });
+
+  afterEach(async () => {
+    writeFile(contentTypeDataModelPath, initialDatamodelContent);
+  });
+});
