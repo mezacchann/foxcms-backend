0831c58517678e20e14389e4f486dc2767b2cf47
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-07-26 14:20:51 +1000

    Store prisma endpoint in environment variable

--- src/prisma/endpoint.provider.ts
+++ src/prisma/endpoint.provider.ts
@@ -1,10 +1,6 @@
-import { promisify } from 'util';
-import * as readYaml from 'read-yaml-promise';
-
 export const endpointProvider = {
   provide: 'PrismaEndpoint',
   useFactory: async () => {
-    const prismaConfig = await readYaml('./prisma/prisma.yml');
-    return prismaConfig.endpoint;
+    return process.env.PRISMA_SERVER_ENDPOINT;
   },
 };
