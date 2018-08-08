b9d5ff85fc8e65d68cc2456b4045fd87aafa6955
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-08 12:40:04 +1000

    Add prisma to apollo server context

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,35 @@
-import { Module } from '@nestjs/common'
+import { Module, Inject } from '@nestjs/common'
 import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql'
 import { ApolloServer } from 'apollo-server-express'
 import { UserModule } from './user/UserModule'
 import { ContentTypeModule } from './content-type/ContentTypeModule'
 import { PrismaModule } from './prisma/PrismaModule'
+import { Prisma } from 'prisma-binding'
+import { RemoteSchema } from './prisma/RemoteSchema'
 
 @Module({
   imports: [GraphQLModule, UserModule, ContentTypeModule, PrismaModule],
 })
 export class AppModule {
-  constructor(private readonly graphQLFactory: GraphQLFactory) {}
+  constructor(
+    private readonly graphQLFactory: GraphQLFactory,
+    @Inject('PrismaSchema') private remoteSchema: RemoteSchema,
+  ) {}
 
   configureGraphQL(app: any) {
     const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql')
     const schema = this.graphQLFactory.createSchema({ typeDefs })
-    const server = new ApolloServer({ schema })
+    const prisma = new Prisma({
+      typeDefs: this.remoteSchema.schemaDefinition,
+      endpoint: process.env.PRISMA_SERVER_ENDPOINT,
+    })
+    const server = new ApolloServer({
+      schema,
+      context: req => ({
+        ...req,
+        prisma,
+      }),
+    })
     server.applyMiddleware({ app })
   }
 }
