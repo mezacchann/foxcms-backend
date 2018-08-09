07696652af3bee08584ff5455c79f649f01f762c
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-09 12:20:38 +1000

    Secure api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as jwt from 'express-jwt'
+import { expressJwtSecret } from 'jwks-rsa'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const auth = jwt({
+    secret: expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://foxcms.auth0.com/.well-known/jwks.json',
+    }),
+    audience: 'https://foxcms/api',
+    issuer: 'https://foxcms.auth0.com/',
+    algorithm: 'RS256',
+  })
+  app.use(auth)
   const appModule = app.get(AppModule)
   appModule.configureGraphQL(app)
   app.enableCors()
