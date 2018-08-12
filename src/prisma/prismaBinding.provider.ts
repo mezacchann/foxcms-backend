4800c1fdcbc7cb6024aceaf2e1a3c345940ceca3
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-12 10:08:39 +1000

    Use secret for app service

--- src/prisma/prismaBinding.provider.ts
+++ src/prisma/prismaBinding.provider.ts
@@ -6,6 +6,7 @@ export const prismaBinding = {
     return new Prisma({
       typeDefs: './prisma/generated/prisma.graphql',
       endpoint: process.env.PRISMA_SERVER_ENDPOINT,
+      secret: process.env.FOXCMS_SECRET,
     })
   },
 }
