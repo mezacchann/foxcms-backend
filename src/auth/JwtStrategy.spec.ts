4ef053c36b668841dbd34a6135a18800a47b571d
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-30 19:15:46 +1000

    Add test for jwt strategy

---
+++ src/auth/JwtStrategy.spec.ts
@@ -0,0 +1,35 @@
+import { Test } from '@nestjs/testing'
+import { JwtStrategy } from './JwtStrategy'
+import { UserService } from '../user/UserService'
+import { UnauthorizedException } from '@nestjs/common'
+
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy
+  let userService: UserService
+  const verifiedCallback = jest.fn()
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [JwtStrategy, UserService, { provide: 'PrismaBinding', useValue: {} }],
+    }).compile()
+
+    jwtStrategy = module.get<JwtStrategy>(JwtStrategy)
+    userService = module.get<UserService>(UserService)
+  })
+
+  afterEach(() => verifiedCallback.mockClear())
+
+  it('should execute the verified function with the resolved user', async () => {
+    const user = { id: 1, username: 'peter' }
+    jest.spyOn(userService, 'getUserById').mockImplementation(() => user)
+    await jwtStrategy.validate({ sub: 1 }, verifiedCallback)
+    expect(verifiedCallback).toBeCalledWith(null, user)
+  })
+
+  it('should execute the verified function with the Unauthorized Exception', async () => {
+    jest.spyOn(userService, 'getUserById').mockImplementation(() => null)
+    await jwtStrategy.validate({ sub: 1 }, verifiedCallback)
+    const exception = new UnauthorizedException()
+    expect(verifiedCallback.mock.calls[0][0].status).toEqual(exception.getStatus())
+  })
+})
