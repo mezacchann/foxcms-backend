83ff7c82411217b443cdd89f5ddd14bdaefc6ddf
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-31 15:37:09 +1000

    Fix dependency mocking

--- src/auth/JwtStrategy.spec.ts
+++ src/auth/JwtStrategy.spec.ts
@@ -2,6 +2,9 @@ import { Test } from '@nestjs/testing'
 import { JwtStrategy } from './JwtStrategy'
 import { UserService } from '../user/UserService'
 import { UnauthorizedException } from '@nestjs/common'
+import { AuthService } from './AuthService'
+import { ProjectService } from '../project/ProjectService'
+import { ContentTypeService } from '../content-type/ContentTypeService'
 
 describe('JwtStrategy', () => {
   let jwtStrategy: JwtStrategy
@@ -10,7 +13,16 @@ describe('JwtStrategy', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [JwtStrategy, UserService, { provide: 'PrismaBinding', useValue: {} }],
+      providers: [
+        JwtStrategy,
+        UserService,
+        UserService,
+        AuthService,
+        ProjectService,
+        ContentTypeService,
+        { provide: 'PrismaManagementToken', useValue: '' },
+        { provide: 'PrismaBinding', useValue: {} },
+      ],
     }).compile()
 
     jwtStrategy = module.get<JwtStrategy>(JwtStrategy)
@@ -22,13 +34,13 @@ describe('JwtStrategy', () => {
   it('should execute the verified function with the resolved user', async () => {
     const user = { id: 1, username: 'peter' }
     jest.spyOn(userService, 'getUserById').mockImplementation(() => user)
-    await jwtStrategy.validate({ sub: 1 }, verifiedCallback)
+    await jwtStrategy.validate({ sub: '1' }, verifiedCallback)
     expect(verifiedCallback).toBeCalledWith(null, user)
   })
 
   it('should execute the verified function with the Unauthorized Exception', async () => {
     jest.spyOn(userService, 'getUserById').mockImplementation(() => null)
-    await jwtStrategy.validate({ sub: 1 }, verifiedCallback)
+    await jwtStrategy.validate({ sub: '1' }, verifiedCallback)
     const exception = new UnauthorizedException()
     expect(verifiedCallback.mock.calls[0][0].status).toEqual(exception.getStatus())
   })
