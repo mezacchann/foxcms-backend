1576b23ef61dec2b0f78653c19c92a1623eb9568
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-31 15:25:36 +1000

    Move logic to service and add unit tests

--- src/user/UserResolver.ts
+++ src/user/UserResolver.ts
@@ -1,67 +1,17 @@
 import { Resolver, Query } from '@nestjs/graphql'
-import { ProjectService } from './../project/ProjectService'
 import { UserService } from './UserService'
-import { AuthService } from '../auth/AuthService'
-import * as bcrypt from 'bcrypt'
-import User from './User'
-import { Project } from 'project/Project'
 
 @Resolver('User')
 export class UserResolver {
-  constructor(
-    private readonly userService: UserService,
-    private readonly authService: AuthService,
-    private readonly projectService: ProjectService,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
   @Query()
   async login(obj, { username, password }, context, info) {
-    const user = await this.userService.getUser(
-      username,
-      '{id username imageUri password salt projects{id}}',
-    )
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      throw new Error('Invalid username or password')
-    }
-    return this.authService.createToken({
-      sub: user.id,
-    })
+    return this.userService.login(username, password)
   }
 
   @Query()
   async signup(obj, { username, password }, context, info) {
-    if (await this.userService.getUser(username)) {
-      throw new Error('Username already in use')
-    }
-    const salt = bcrypt.genSaltSync()
-    const user = (await context.prisma.mutation.createUser(
-      {
-        data: {
-          username,
-          password: bcrypt.hashSync(password, salt),
-          salt,
-        },
-      },
-      '{id username imageUri}',
-    )) as User
-    const projectName = await this.projectService.buildProject()
-    await context.prisma.mutation.createProject(
-      {
-        data: {
-          user: {
-            connect: {
-              id: user.id,
-            },
-          },
-          providedName: 'initial-project',
-          generatedName: projectName,
-          stage: 'Production',
-        },
-      },
-      '{id}',
-    )
-    return this.authService.createToken({
-      sub: user.id,
-    })
+    return this.userService.signup(username, password)
   }
 }
--- src/user/UserService.ts
+++ src/user/UserService.ts
@@ -1,10 +1,49 @@
 import { Injectable, Inject } from '@nestjs/common'
-import { Prisma } from 'prisma-binding'
+import * as bcrypt from 'bcrypt'
 import User from './User'
+import { AuthService } from '../auth/AuthService'
+import { Prisma } from '../typings/prisma'
+import { ProjectService } from '../project/ProjectService'
 
 @Injectable()
 export class UserService {
-  constructor(@Inject('PrismaBinding') private prismaBinding: Prisma) {}
+  constructor(
+    @Inject('PrismaBinding') private prismaBinding: Prisma,
+    private readonly authService: AuthService,
+    private readonly projectService: ProjectService,
+  ) {}
+
+  async login(username: string, password: string) {
+    const user = await this.getUser(username, '{id password')
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      throw new Error('Invalid username or password')
+    }
+    return this.authService.createToken({
+      sub: user.id.toString(),
+    })
+  }
+
+  async signup(username: string, password: string) {
+    if (await this.getUser(username)) {
+      throw new Error('Username already in use')
+    }
+    const salt = bcrypt.genSaltSync()
+    const user = await this.prismaBinding.mutation.createUser(
+      {
+        data: {
+          username,
+          password: bcrypt.hashSync(password, salt),
+          salt,
+        },
+      },
+      '{id}',
+    )
+    await this.projectService.buildProject(user.id)
+    return this.authService.createToken({
+      sub: user.id,
+    })
+  }
+
   getUser(username: string, info: any = '{id}'): Promise<User> {
     return this.prismaBinding.query.user(
       {
@@ -16,7 +55,7 @@ export class UserService {
     )
   }
 
-  getUserById(id: number, info: any = '{id}'): Promise<User> {
+  getUserById(id: string, info: any = '{id}'): Promise<User> {
     return this.prismaBinding.query.user(
       {
         where: {
