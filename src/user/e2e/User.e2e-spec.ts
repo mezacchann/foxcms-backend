94450940a4a782318625d9608596390576d5a65d
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-09-01 14:31:25 +1000

    Add sample e2e test

---
+++ src/user/e2e/User.e2e-spec.ts
@@ -0,0 +1,32 @@
+import * as request from 'supertest'
+import { Test } from '@nestjs/testing'
+import { UserModule } from '../UserModule'
+import { INestApplication } from '@nestjs/common'
+import { AuthModule } from '../../auth/AuthModule'
+import { AppModule } from '../../app.module'
+import { UserService } from '../UserService'
+
+describe('Cats', () => {
+  let app: INestApplication
+  let userService: UserService
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule, UserModule, AuthModule],
+    }).compile()
+
+    app = module.createNestApplication()
+    userService = module.get<UserService>(UserService)
+    await app.init()
+  })
+
+  it(`should create a user and return the token`, async () => {
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: 'query{signup(username: "peter2", password: "his-secret")}' })
+    console.log(await userService.getUser('peter'))
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+})
