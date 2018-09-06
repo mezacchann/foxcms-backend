844ec4686f277f34f473d3db906ecaa19756db07
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-09-06 14:02:58 +1000

    Add project guard

--- src/file/FileController.ts
+++ src/file/FileController.ts
@@ -4,11 +4,15 @@ import {
   UseInterceptors,
   FileInterceptor,
   UploadedFile,
+  UseGuards,
 } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { ProjectGuard } from '../project/ProjectGuard'
 
 @Controller('file')
+@UseGuards(AuthGuard('jwt'), ProjectGuard)
 export class FileController {
-  @Post('upload')
+  @Post(':projectId')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file) {
     console.log(file)
