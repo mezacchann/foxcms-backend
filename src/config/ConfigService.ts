8ddd1ac29901b8ff83d5f49ad05f3a13ecdd9efc
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-09-07 10:41:02 +1000

    Add config module

---
+++ src/config/ConfigService.ts
@@ -0,0 +1,49 @@
+import * as Joi from 'joi'
+import * as fs from 'fs'
+import * as dotenv from 'dotenv'
+export interface EnvConfig {
+  [key: string]: string
+}
+
+export class ConfigService {
+  private readonly envConfig: EnvConfig
+
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath))
+    this.envConfig = this.validateInput(config)
+  }
+
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(['development', 'production', 'test'])
+        .default('development'),
+      PRISMA_SERVER: Joi.string().required(),
+      FOXCMS_SECRET: Joi.string().required(),
+      PRISMA_MANAGEMENT_API_SECRET: Joi.string().required(),
+      UPLOAD_DIR: Joi.string().required(),
+    })
+
+    const { error, value: validatedEnvConfig } = Joi.validate(envConfig, envVarsSchema)
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`)
+    }
+    return validatedEnvConfig
+  }
+
+  get nodeEnv(): string {
+    return String(this.envConfig.NODE_ENV)
+  }
+
+  get foxCmsSecret(): string {
+    return String(this.envConfig.FOXCMS_SECRET)
+  }
+
+  get prismaManagementSecret(): string {
+    return String(this.envConfig.PRISMA_MANAGEMENT_API_SECRET)
+  }
+
+  get uploadDir(): string {
+    return String(this.envConfig.UPLOAD_DIR)
+  }
+}
